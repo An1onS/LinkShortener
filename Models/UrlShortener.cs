b46bab3e1dbd7831a15357900ac69a431512e3a0
Anton Sakharov <ya.antonsakharov@yandex.ru>
2019-09-08 16:38:23 +0500

    short url by base64hash

---
+++ Models/UrlShortener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Models
+{
+	public static class UrlShortener
+	{
+		static string GetShortUrl(string url)
+		{
+			var hash = url.GetHashCode();
+			return Convert.ToBase64String(BitConverter.GetBytes(hash));
+		}
+	}
+}
